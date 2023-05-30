9983a216b9d07b1143e1ac402c12c7a69c392821
Anton Waniek <antonwnk@users.noreply.github.com>
2023-05-30 16:45:21 +0300

    change java to ftplugin, fix mason load err

--- lua/pluginmanager.lua
+++ lua/pluginmanager.lua
@@ -34,7 +34,7 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
+      { 'williamboman/mason.nvim', opts = {} },
       'williamboman/mason-lspconfig.nvim',
 
       -- Useful status updates for LSP
